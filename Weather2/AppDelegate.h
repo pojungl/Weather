ef8fd384d1c774342e8fcbcd4478a582ba93ab7f
PO-JUNG <po-jung@localhost>
2016-11-03 20:26:28 +0800

    Initial Commit

---
+++ Weather2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather2
+//
+//  Created by PO-JUNG on 16/11/3.
+//  Copyright © 2016年 PO-JUNG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Weather2
+//
+//  Created by PO-JUNG on 16/11/3.
+//  Copyright © 2016年 PO-JUNG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
